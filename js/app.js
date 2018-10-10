370d0ab84fc3d5f90fba150635259f6fc0996f12
M Aakif Raza <simply.aakif@gmail.com>
2018-10-10 17:03:07 +0500

    Course Details Page & Pe-icon-7

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+    $('.bigMenu').click(function(){
+        $('.bigMenu').toggleClass('active');
+        $('.overlay').toggleClass('active');
+    })
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,15 @@
 
 let mix = require('laravel-mix');
+let font = require('laravel-mix');
 
 mix.sass('sass/app.scss', 'css/ace-style.css')
     .options({
         processCssUrls: false
+    }); 
+
+font.sass('node_modules/pixeden-stroke-7-icon/pe-icon-7-stroke/scss/pe-icon-7-stroke.scss','css/picon-7.css',{
+        // outputStyle: 'compressed'
+    })
+    .options({
+        processCssUrls: true
     }); 
